ccb388e2aae52774655d9cd8c567b0ea07370b02
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-16 10:48:56 -0300

    server configs

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { getAccountById, getAllAccounts } from "./usecases/AccountController";
+import { valueTransfer } from "./usecases/TransferController";
+
+const router = Router()
+
+router.get('/api/Account', getAllAccounts)
+router.get('/api/Account/:id', getAccountById)
+
+router.get('api/fund-transfer/:id')
+router.post('/api/fund-transfer', valueTransfer)
+
+export { router }
