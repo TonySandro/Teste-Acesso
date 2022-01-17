ae06da2223776f7be4f405c7589920bdcaa04081
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-17 14:17:25 -0300

    moved repository features

--- src/controller/transactionController.ts
+++ src/controller/transactionController.ts
@@ -1,10 +1,4 @@
-import { Request, Response } from "express"
-import TransactionSchema from "../Schemas/transactionSchema"
-
-
-
 export class TransactionController {
-
     // Função de validação
     public transactionValidate(
         accountOrigin: string,
@@ -20,21 +14,5 @@ export class TransactionController {
         }
         return false
     }
-
-    public async index(req: Request, res: Response) {
-        const transactions = await TransactionSchema.find()
-
-        return res.json(transactions)
-    }
-
-    public async store(req: Request, res: Response) {
-        const transactions = req.body
-        // const transactions = await TransactionSchema.create(req.body)
-
-        console.log(transactions)
-
-        return res.json(transactions)
-    }
-
 
 }
