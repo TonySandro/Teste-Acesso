0d3f1c04e0483ef775aac3ad9459fd6e7609b85e
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-17 15:01:09 -0300

    update transactions

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { api } from "../api";
 import { TransactionController } from "../controller/transactionController";
-import { getDataByAccountNumber } from "./http/getAccountData";
+import { CreateTransactions } from "../repositories/services/createTransaction";
+import { getDataByAccountNumber } from "./http/getDataAccount";
 import { postCreditOrDebit } from "./http/postCreditOrDebit";
 
 const transactionController = new TransactionController()
+const createTransactions = new CreateTransactions()
 
 export class TransactionService {
     async valueTransaction(req: Request, res: Response) {
@@ -13,19 +15,23 @@ export class TransactionService {
 
             let accountOriginVerify = getDataByAccountNumber(accountOrigin)
             let accountDestinationVerify = getDataByAccountNumber(accountDestination)
-
             Promise.all([accountOriginVerify, accountDestinationVerify])
 
-            if (accountOriginVerify !== undefined && accountDestinationVerify !== undefined) {
-                const result = transactionController.transactionValidate(accountOrigin, accountDestination, value)
-                if (result) {
-                    let debit = postCreditOrDebit(accountOrigin, value, "Debit")
-                    let credit = postCreditOrDebit(accountOrigin, value, "Credit")
+            let accOrigin = (await Promise.resolve(accountOriginVerify)).status
 
-                    Promise.all([debit, credit])
+            if ((await Promise.resolve(accountOriginVerify)).status !== 200 &&
+                (await Promise.resolve(accountDestinationVerify)).status !== 200) {
+                const result = transactionController.transactionValidate(accountOrigin, accountDestination, value, 200)
+                //If validation returns true
+                if (result) {
+                    //Carry out the credit and debit transaction
+                    await postCreditOrDebit(accountOrigin, accountDestination, value)
                 }
             }
-            return res.status(200).json("Transactionencia realizada.")
+
+            //Send the transaction to the database and return the transactionId
+            const transaction = createTransactions.insertTransaction(req.body)
+            return res.status(200).json((await transaction).transactionId)
         } catch (error) {
             res.json({
                 message: error.message || 'Unexpected error.'
