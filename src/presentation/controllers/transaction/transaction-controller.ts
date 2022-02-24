ba5b5c774846fe64c690f9f3914436ba266c7d9b
TonySandro <tonysduarte101@gmail.com>
2022-02-24 16:49:51 -0300

    refactor: make TransactionController protocols

--- src/presentation/controllers/transaction/transaction-controller.ts
+++ src/presentation/controllers/transaction/transaction-controller.ts
@@ -1,8 +1,8 @@
-import { badRequest } from "../..//helpers/http/http-helper"
+import { HttpRequest, HttpResponse, Controller } from "../../protocols"
 import { MissingParamError } from "../../errors/missing-param-error"
-import { HttpRequest, HttpResponse } from "../../protocols/http"
+import { badRequest } from "../..//helpers/http/http-helper"
 
-export class TransactionController {
+export class TransactionController implements Controller {
     handle(httpRequest: HttpRequest): HttpResponse {
         const requiredFields = ['accountOrigin', 'accountDestination', 'value']
 
