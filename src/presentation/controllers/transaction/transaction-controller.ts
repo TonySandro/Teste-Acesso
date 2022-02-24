0471addedf5e2d6d3548accfd00b836ea4eb87e2
TonySandro <tonysduarte101@gmail.com>
2022-02-24 15:57:59 -0300

    feat: ensure TransactionController returns 400 if no accountDestination is provided

--- src/presentation/controllers/transaction/transaction-controller.ts
+++ src/presentation/controllers/transaction/transaction-controller.ts
@@ -1,9 +1,13 @@
 export class TransactionController {
     handle(httpRequest: any): any {
+        let errorParam = 'accountOrigin'
 
+        if (!httpRequest.body.accountDestination) {
+            errorParam = 'accountDestination'
+        }
         return {
             statusCode: 400,
-            body: new Error('Missing param: accountOrigin')
+            body: new Error(`Missing param: ${errorParam}`)
         }
     }
 }
