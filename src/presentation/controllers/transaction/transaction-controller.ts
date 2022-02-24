1066a33a9122eb385176f8dcf42d6aed480860cb
TonySandro <tonysduarte101@gmail.com>
2022-02-24 16:03:47 -0300

    refactor: ensure TransactionController.handle implement correct interfaces

--- src/presentation/controllers/transaction/transaction-controller.ts
+++ src/presentation/controllers/transaction/transaction-controller.ts
@@ -1,10 +1,13 @@
+import { HttpRequest, HttpResponse } from "@presentation/protocols/http"
+
 export class TransactionController {
-    handle(httpRequest: any): any {
+    handle(httpRequest: HttpRequest): HttpResponse {
         let errorParam = 'accountOrigin'
 
         if (!httpRequest.body.accountDestination) {
             errorParam = 'accountDestination'
         }
+
         return {
             statusCode: 400,
             body: new Error(`Missing param: ${errorParam}`)
