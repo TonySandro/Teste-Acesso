49ab511f4596867541990eee36dd656a629d7553
TonySandro <tonysduarte101@gmail.com>
2022-03-03 11:06:22 -0300

    feat: ensure TransactionMongoRepository returns an transaction on success

---
+++ src/infra/db/mongodb/transaction-repository/transaction.ts
@@ -0,0 +1,14 @@
+import { TransactionModel } from "domain/models/transaction";
+import { AddTransactionModel } from "domain/usecases/add-transaction";
+import { MongoHelper } from "../helpers/mongo-helper";
+
+export class TransactionMongoRepository {
+    async addTransaction(transactionData: AddTransactionModel): Promise<TransactionModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        const result = await accountCollection.insertOne(transactionData)
+        const transaction = await accountCollection.findOne(result.insertedId)
+        console.log(transaction)
+        return MongoHelper.map(transaction);
+    }
+
+}
