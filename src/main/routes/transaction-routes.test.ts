54d031a2de5d1878c33fe961a5732581fdc8f1e3
TonySandro <tonysduarte101@gmail.com>
2022-03-09 15:31:19 -0300

    feat: connect to a mongodb

--- src/main/routes/transaction-routes.test.ts
+++ src/main/routes/transaction-routes.test.ts
@@ -1,7 +1,21 @@
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '../config/app'
 
 describe('Transaction Routes', () => {
+
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        const transactionCollection = MongoHelper.getCollection('transaction')
+        await (await transactionCollection).deleteMany({})
+    })
     test('Should return an account on success', async () => {
         await request(app)
             .post('/api/fund-transfer')
