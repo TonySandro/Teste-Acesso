1bd33f1392d1b175a9dd7859c07359c973f6d8eb
TonySandro <tonysduarte101@gmail.com>
2022-03-11 22:33:02 -0300

    feat: add axios

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
     mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/teste-acessobankly',
     port: process.env.PORT || 3030,
+    apiUrl: 'https://acessoaccount.herokuapp.com/api'
 }
