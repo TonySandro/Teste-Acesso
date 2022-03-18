7b1bef73e439e5dbec766a6e04a150ba0104d980
TonySandro <tonysduarte101@gmail.com>
2022-03-18 00:33:59 -0300

    test: ensure ConsultationController call readTransaction with correct values

--- src/presentation/controllers/consultation/consultation-controller.spec.ts
+++ src/presentation/controllers/consultation/consultation-controller.spec.ts
@@ -1,10 +1,37 @@
+import { ConsultationModel, ReadTransaction } from "../../../domain/usecases/read-transaction"
 import { MissingParamError } from "../../../presentation/errors"
 import { ConsultationController } from "./consultation-controller"
+import { HttpRequest } from "./consultation-controller-protocols"
 
-const makeSut = () => {
-    const sut = new ConsultationController()
+const makeFakeRequest = (): HttpRequest => ({
+    body: {
+        transactionId: "valid_transaction_id",
+    }
+})
+
+const makeReadTransaction = (): ReadTransaction => {
+    class ReadTransactionStub implements ReadTransaction {
+        read(transactionId: string): Promise<ConsultationModel> {
+            return new Promise(resolve => resolve({
+                transactionId: "valid_transaction_id",
+                status: "Confirmed"
+            }))
+        }
+    }
+    return new ReadTransactionStub()
+}
+
+interface SutTypes {
+    sut: ConsultationController
+    readTransaction: ReadTransaction
+}
+
+const makeSut = (): SutTypes => {
+    const readTransaction = makeReadTransaction()
+    const sut = new ConsultationController(readTransaction)
     return {
-        sut
+        sut,
+        readTransaction
     }
 }
 
@@ -24,11 +51,7 @@ describe('Consultation Controller', () => {
 
     test('Should return 200 if valid data is provided', async () => {
         const { sut } = makeSut()
-        const httpRequest = {
-            body: {
-                transactionId: "valid_transaction_id",
-            }
-        }
+        const httpRequest = makeFakeRequest()
 
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(200)
@@ -36,5 +59,12 @@ describe('Consultation Controller', () => {
             transactionId: "valid_transaction_id",
             status: "Confirmed" || "Error"
         })
+    })
+
+    test('Should call readTransaction with correct values', async () => {
+        const { sut, readTransaction } = makeSut()
+        const readTransactionSpy = jest.spyOn(readTransaction, "read")
+        await sut.handle(makeFakeRequest())
+        expect(readTransactionSpy).toHaveBeenCalledWith("valid_transaction_id")
     })
 })
