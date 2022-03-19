943c26720b427c446d28d0c5c66406348d7bcced
TonySandro <tonysduarte101@gmail.com>
2022-03-19 12:04:33 -0300

    test: ensure ConsultationController return 500 if readTransaction throws

--- src/presentation/controllers/consultation/consultation-controller.spec.ts
+++ src/presentation/controllers/consultation/consultation-controller.spec.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, ConsultationModel, ReadTransaction } from "./consultation-controller-protocols"
-import { MissingParamError } from "../../../presentation/errors"
 import { ConsultationController } from "./consultation-controller"
+import { MissingParamError, ServerError } from "../../../presentation/errors"
+import { serverError } from "../../../presentation/helpers/http/http-helper"
 
 const makeFakeRequest = (): HttpRequest => ({
     body: {
@@ -65,5 +66,16 @@ describe('Consultation Controller', () => {
         const readTransactionSpy = jest.spyOn(readTransaction, "read")
         await sut.handle(makeFakeRequest())
         expect(readTransactionSpy).toHaveBeenCalledWith("valid_transaction_id")
+    })
+
+    test('Should return 500 if readTransaction throws', async () => {
+        const { sut, readTransaction } = makeSut()
+        jest.spyOn(readTransaction, "read").mockImplementationOnce(() => {
+            return new Promise((resolve, reject) => reject(new Error()))
+        })
+
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body.Message).toEqual(new ServerError(new Error()))
     })
 })
