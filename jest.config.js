bbcd8721e801dfdcf2a01f48b1badf37164fe52d
TonySandro <tonysduarte101@gmail.com>
2022-03-31 14:23:07 -0300

    refactor: update jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   roots: ["<rootDir>/src"],
   // clearMocks: true,
   // collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/**/*-protocols.ts",
+    "!<rootDir>/src/**/index.ts",
+  ],
   coverageDirectory: "coverage",
   // coverageProvider: "v8",
   testEnvironment: "node",
